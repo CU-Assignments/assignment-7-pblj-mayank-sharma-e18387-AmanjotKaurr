a2be1f9b533893d5bca788008b3cdb8504479346
Amanjot <155871189+AmanjotKaurr@users.noreply.github.com>
2025-04-11 21:50:21 +0530

    Create 22BCS16416_Hard-StudentAttendance_Model Class.java

---
+++ 22BCS16416_Hard-StudentAttendance_Model Class.java
@@ -0,0 +1,15 @@
+public class StudentAttendance {
+    private String name;
+    private String date;
+    private String status;
+
+    public StudentAttendance(String name, String date, String status) {
+        this.name = name;
+        this.date = date;
+        this.status = status;
+    }
+
+    public String getName() { return name; }
+    public String getDate() { return date; }
+    public String getStatus() { return status; }
+}
